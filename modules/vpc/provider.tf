07c169433eb7ccb1513b13051d0ba967ec2996aa
u631665 <cagarcia@teco.com.ar>
2023-04-23 00:22:37 -0300

    first commit

---
+++ modules/vpc/provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region_name
+  default_tags {
+    tags = {
+      "Application" = "Kubernetes"
+      "Tool"        = "Terragrunt"
+    }
+  }
+}
