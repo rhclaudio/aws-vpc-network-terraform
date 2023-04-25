563d148d51d2ce26557b6d32f6c4b0aeaad84a22
u631665 <cagarcia@teco.com.ar>
2023-04-25 09:26:57 -0300

    moving subnets variables configurations from list to map

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -7,13 +7,11 @@ locals {
 variable "region_name" {
   description = "Region name to launch VPC network"
   type        = string
-  default     = "us-east-1"
 }
 
 variable "vpc_cidr_block" {
   description = "VPC network CIDR"
   type        = string
-  default     = "10.10.0.0/16"
 }
 
 variable "environment" {
@@ -25,22 +23,12 @@ variable "environment" {
 
 variable "public_subnets" {
   description = "Provider public subnets CIDR values"
-  #type        = map(any)
-  type = list(string)
-//  default = [
-//    "10.10.0.0/24",
-//    "10.10.2.0/24"
-//  ]
+  type        = map(any)
 }
 
 variable "private_subnets" {
   description = "Provide private subnets CIDR values"
-  #type        = map(any)
-  type = list(string)
-//  default = [
-//    "10.10.1.0/24",
-//    "10.10.3.0/24"
-//  ]
+  type        = map(any)
 }
 
 variable "pub_sub_tags" {
